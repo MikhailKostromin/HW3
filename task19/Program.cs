72950469be82e9fc1bb0a66fa8ebaeade51fdf8b
Mikhail <kostrominmi@mail.ru>
2022-08-28 23:20:21 +0300

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+String Polinrome ()
+{
+    Console.WriteLine("Введите пятизначное число ");
+    int x= Convert.ToInt32(Console.ReadLine());
+    string res = string.Empty;   //пустая строка
+    if (x%10==x/10000%10 & x/10%10==x/1000%10)
+    {
+        Console.WriteLine("Палиндром ");
+    }
+    else 
+    { 
+        Console.WriteLine(" Не Палиндром ");
+    }
+    return res;
+}
+
+try 
+{    
+    Console.WriteLine(Polinrome ()); 
+} 
+catch 
+{     
+    Console.WriteLine("Необходимо ввести пять положительных целых числа");     
+}
